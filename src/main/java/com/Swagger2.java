175bbb153fb484a446d9f8b6ea6927ea92fe0843
lixiaochun <lxc9349@163.com>
2019-01-11 16:39:46 +0800

    frist

--- src/main/java/com/Swagger2.java
+++ src/main/java/com/Swagger2.java
@@ -1,12 +1,14 @@
 package com;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -26,11 +28,16 @@ public class Swagger2 {
     }
 
     private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Spring Boot添加Swagger2组件")
-                .description("Spring Boot添加Swagger2组件")
-                .version("1.0")
-                .build();
+    	return new ApiInfo(
+                "TITLE",
+                "DESCIPRION",
+                "VERSION",
+                "TERMS OF SERVICE URL",
+                new Contact("NAME","URL","EMAIL"),
+                "LICENSE",
+                "LICENSE URL",
+                Collections.emptyList()
+        );
     }
 
 }
